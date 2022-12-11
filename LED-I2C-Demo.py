06c2b5b6efc0794fc436785b5851366edc8a6a54
Ayotie Channel <117144390+rachmansyahiot@users.noreply.github.com>
2022-12-11 08:01:29 +0700

    Add files via upload

---
+++ LED-I2C-Demo.py
@@ -0,0 +1,23 @@
+import machine
+from machine import Pin, SoftI2C
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+from time import sleep
+
+I2C_ADDR = 0x27
+totalRows = 2
+totalColumns = 16
+
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)     #initializing the I2C method for ESP32
+#i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)       #initializing the I2C method for ESP8266
+
+lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
+
+while True:
+    lcd.move_to(0,0)
+    lcd.putstr("Ayotie channel ")
+    sleep(2)
+    lcd.move_to(0,1)
+    lcd.putstr("Ayo Belajar IoT")
+    sleep(2)
+    lcd.clear()
